4fbc154f6c3969b33256738f5f4ba13f7b09bf20
CarpetToaster <davidjosephibrahim@gmail.com>
2025-03-23 15:56:54 -0600

    Started working on part 2

---
+++ my-lms-app/src/data/courses.js
@@ -0,0 +1,35 @@
+const courses = [
+    {
+        id: 1,
+        name: "Web Development",
+        instructor: "Dr. John Smith",
+        description: "Master HTML, CSS, and JavaScript.",
+        duration: "8 weeks",
+        image: "images/course1.jpg"
+    },
+    {
+        id: 2,
+        name: "Object Oriented Principles",
+        instructor: "Dr. Jane Doe",
+        description: "Learn how to code with object oriented design principles in mind",
+        duration: "8 weeks",
+        image: "images/course1.jpg"
+    },
+    {
+        id: 3,
+        name: "Differential Equations",
+        instructor: "Dr. L",
+        description: "Ordinary differential equations, Laplace transform, Fourier series",
+        duration: "8 weeks",
+        image: "images/course2.jpg"
+    },
+    {
+        id: 4,
+        name: "Intro to Computer Graphics",
+        instructor: "Dr. Aarav Singh",
+        description: "Affine transformations and coordinate models, ray tracing, and meshing",
+        duration: "8 weeks",
+        image: "images/course1.jpg"
+    }
+];
+    export default courses;
