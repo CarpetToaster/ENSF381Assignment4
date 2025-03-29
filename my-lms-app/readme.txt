ce372a934a604c8873a95f4f006419f063c285cf
CarpetToaster <davidjosephibrahim@gmail.com>
2025-03-29 15:43:31 -0600

    I think everything is done; all the requirements seem met?

--- my-lms-app/readme.txt
+++ my-lms-app/readme.txt
@@ -7,5 +7,6 @@ my-lms-app@0.1.0 C:\Users\david\Desktop\ENSF 381\assignments\assignment 4\ENSF38
 +-- react-router-dom@7.4.0
 +-- react-scripts@5.0.1
 +-- react@19.0.0
++-- styled-components@6.1.16
 `-- web-vitals@2.1.4
 
--- my-lms-app/src/index.js
+++ my-lms-app/src/index.js
@@ -1,10 +1,12 @@
+// Ayre Ibrahim (UCID#30207941)
+// Mykola Viktorovskyi (UCID#30233216)
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Homepage from './components/Homepage.js';
 import App from "./App.js"
-// import CoursesPage from "./components/CoursesPage.js"
-// import LoginFOrm from "./components/LoginForm.js"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
